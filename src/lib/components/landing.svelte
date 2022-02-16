d467179fee4efe67d4b0bce02788d88e948b4500
Biruk Tesfaye <biruktesfayeve@gmail.com>
2022-02-16 16:30:06 +0300

    implement copy functionality and fix generate function

--- src/lib/components/landing.svelte
+++ src/lib/components/landing.svelte
@@ -1,7 +1,66 @@
-<script lg="ts">
+<script lang="ts">
 	import githubIcon from '$lib/assets/icons/github.svg';
 	import copyIcon from '$lib/assets/icons/copy-icon.svg';
+
+	import CopyToClipboard from '$lib/utils/copy-to-clipboard.svelte';
+
+	let copytext = '';
+	let amount: number = 1;
+	let start = false;
+	let scope = 'WORDS';
+	let resScope = 'WORDS';
+
+	let submit = false;
+
+	let generatedData = [];
+
+	function generateLorem() {
+		submit = true;
+		fetch('/api/generate', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				amount: amount,
+				start: start,
+				scope: scope
+			})
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				submit = false;
+				generatedData = data.generated;
+				resScope = data.scope;
+
+				if (data.scope === 'WORDS') {
+					copytext = data.generated.join(' ');
+				} else if (data.scope === 'PARAGRAPHS') {
+					copytext = data.generated.join('\n');
+				} else {
+					copytext = data.generated.join('');
+				}
+			})
+			.catch(() => {
+				console.error;
+				submit = false;
+			});
+	}
+
+	const copy = () => {
+		const app = new CopyToClipboard({
+			target: document.getElementById('clipboard'),
+			props: {
+				text: copytext
+			}
+		});
+		app.$destroy();
+	};
 </script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
 
 <div class="w-full h-screen flex">
 	<div class={'w-[410px] h-screen bg-white p-8 flex-shrink-0'}>
@@ -21,8 +80,14 @@
 
 				<div class="grid gap-8">
 					<h4 class={'text-2xl text-primary-default'}>Generate random amharic placeholder text</h4>
-					<div class="grid gap-8">
-						<input type="number" class="pl-2 w-full h-10 border rounded bg-bg-light" />
+					<form on:submit|preventDefault={generateLorem} class="grid gap-8">
+						<input
+							type="number"
+							bind:value={amount}
+							min="1"
+							max="100"
+							class="pl-2 w-full h-10 border rounded bg-bg-light"
+						/>
 						<div class="grid gap-4">
 							<!-- three radio inputs -->
 							<label
@@ -35,7 +100,8 @@
 									id="words"
 									name="scope"
 									value="WORDS"
-									class="peer hidden "
+									class="peer hidden"
+									bind:group={scope}
 								/>
 								<span
 									class="input-span peer-checked:bg-primary-ex-light peer-checked:before:shadow-input-checked"
@@ -52,6 +118,7 @@
 									name="scope"
 									value="SENTENCES"
 									class="peer hidden "
+									bind:group={scope}
 								/>
 								<span
 									class="input-span peer-checked:bg-primary peer-checked:before:shadow-input-checked"
@@ -68,6 +135,7 @@
 									name="scope"
 									value="PARAGRAPHS"
 									class="peer hidden "
+									bind:group={scope}
 								/>
 								<span
 									class="input-span peer-checked:bg-primary peer-checked:before:shadow-input-checked"
@@ -83,6 +151,7 @@
 									id="startWithAbebe"
 									name="startWithAbebe"
 									class="peer hidden"
+									bind:checked={start}
 								/>
 								<span
 									class="input-checkbox peer-checked:bg-primary peer-checked:before:shadow-input-checked"
@@ -91,8 +160,10 @@
 							</label>
 						</div>
 
-						<button class="w-full bg-primary-default px-4 py-3 rounded text-white">Generate</button>
-					</div>
+						<button type="submit" class="w-full bg-primary-default px-4 py-3 rounded text-white"
+							>Generate</button
+						>
+					</form>
 				</div>
 			</div>
 
@@ -103,9 +174,11 @@
 		</div>
 	</div>
 
-	<div class={'h-screen w-full bg-bg-light '}>
-		<div class="max-w-[720px] w-3/4  my-0 mx-auto pt-32 grid gap-16">
-			<div class="grid  gap-6">
+	<div class={'h-screen w-full bg-bg-light pb-16 pt-32'}>
+		<div
+			class="max-w-[720px] w-3/4 max-h-[calc(100vh-192px)] overflow-y-hidden relative   my-0 mx-auto   flex flex-col"
+		>
+			<div class="grid  gap-6 mb-16">
 				<h1 class=" text-7xl">Abebe Beso Bela</h1>
 
 				<p class="text-lg ">
@@ -115,12 +188,34 @@
 				</p>
 			</div>
 
-			<div class="bg-white flex flex-col p-4 rounded-lg text-primary-default">
+			<div
+				class="bg-white overflow-y-auto  flex-1  flex flex-col p-4 rounded-lg text-primary-default"
+			>
 				<!-- copy icon -->
-				<div class="flex justify-center items-center self-end mb-4">
+				<button on:click={copy} class=" justify-center items-center self-end mb-4">
 					<img src={copyIcon} alt="copy icon" />
+				</button>
+				<div id={'clipboard'} class="w-full  ">
+					{#if submit}
+						<div>loading...</div>
+					{:else if generatedData.length === 0}
+						<div>
+							<p>your generated text will be here</p>
+						</div>
+					{:else if generatedData.length > 0}
+						<div>
+							{#if resScope === 'PARAGRAPHS'}
+								{#each generatedData as data}
+									<p>{start ? `አበበ በሶ በላ ${data}` : data} <br /><br /></p>
+								{/each}
+							{:else if resScope === 'WORDS'}
+								{generatedData.join(' ')}
+							{:else}
+								{start ? `አበበ በሶ በላ ${generatedData.join('')}` : generatedData.join('')}
+							{/if}
+						</div>
+					{/if}
 				</div>
-				<p class="w-full">your generated text will be here</p>
 			</div>
 		</div>
 	</div>
--- src/routes/api/generate.ts
+++ src/routes/api/generate.ts
@@ -1,102 +1,91 @@
-
 import words from '$lib/data/_.json';
 export type IGenerate = {
-    amount: number
-    start: boolean
-    scope: 'PARAGRAPH' | 'WORDS'
-}
+	amount: number;
+	start: boolean;
+	scope: 'PARAGRAPHS' | 'WORDS' | 'SENTENCES';
+};
 
-const MAX_WORDS_PER_SENTENCE = 15
-const MIN_WORDS_PER_SENTENCE = 7
+const MAX_WORDS_PER_SENTENCE = 15;
+const MIN_WORDS_PER_SENTENCE = 7;
 
-const MAX_SENTENCES_PER_PARAGRAPH = 10
-const MIN_SENTENCES_PER_PARAGRAPH = 3
+const MAX_SENTENCES_PER_PARAGRAPH = 10;
+const MIN_SENTENCES_PER_PARAGRAPH = 3;
 
 export async function generate(body: IGenerate) {
-    const { amount, start, scope } = body
-    let generated = []
-    // if start is true then generated will start with 'Abebe beso bela'
-    if (start) {
-        generated.push('Abebe beso bela ')
-    }
+	const { amount, start, scope } = body;
+	let generated = [];
 
-    //  if scope is paragraph then generated will contain amount of paragraphs
-    if (scope === 'PARAGRAPH') {
-        for (let i = 0; i < amount; i++) {
-            generated.push(`${generateParagraph()} `)
-        }
-    }
-    // if scope is word then generated will contain amount of words
-    if (scope === 'WORDS') {
-        for (let i = 0; i < amount; i++) {
-            generated.push(`${words[Math.floor(Math.random() * words.length)]} `)
-        }
-    }
-    return generated
+	//  if scope is paragraph then generated will contain amount of paragraphs
+	if (scope === 'PARAGRAPHS') {
+		for (let i = 0; i < amount; i++) {
+			generated.push(`${generateParagraph()} `);
+		}
+	}
+
+	// if scope is sentence then generated will contain amount of sentences
+	if (scope === 'SENTENCES') {
+		for (let i = 0; i < amount; i++) {
+			generated.push(`${generateSentence()} `);
+		}
+	}
+	// if scope is word then generated will contain amount of words
+	if (scope === 'WORDS') {
+		for (let i = 0; i < amount; i++) {
+			generated.push(`${words[Math.floor(Math.random() * words.length)]} `);
+		}
+	}
+	return generated;
 }
 
-
 function generateParagraph() {
-    const paragraph = []
-    const sentence = getRandomNumber(MIN_SENTENCES_PER_PARAGRAPH, MAX_SENTENCES_PER_PARAGRAPH)
+	const paragraph = [];
+	const sentence = getRandomNumber(MIN_SENTENCES_PER_PARAGRAPH, MAX_SENTENCES_PER_PARAGRAPH);
 
-    for (let i = 0; i < sentence; i++) {
-        paragraph.push(`${generateSentence()}`)
-    }
-    return paragraph.join('\n')
+	for (let i = 0; i < sentence; i++) {
+		paragraph.push(`${generateSentence()}`);
+	}
+	return paragraph.join('\n');
 }
 
 function generateSentence() {
-    const sentence = []
-    const word = getRandomNumber(MIN_WORDS_PER_SENTENCE, MAX_WORDS_PER_SENTENCE)
+	const sentence = [];
+	const word = getRandomNumber(MIN_WORDS_PER_SENTENCE, MAX_WORDS_PER_SENTENCE);
 
-    for (let i = 0; i < word; i++) {
-        sentence.push(`${words[Math.floor(Math.random() * words.length)]} `)
-    }
+	for (let i = 0; i < word; i++) {
+		sentence.push(`${words[Math.floor(Math.random() * words.length)]} `);
+	}
 
-    sentence[sentence.length - 1] = sentence[sentence.length - 1].replace(' ', '.')
+	sentence[sentence.length - 1] = sentence[sentence.length - 1].replace(' ', '።');
 
-    return `${sentence.join('')}`
+	return `${sentence.join('')}`;
 }
-
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
-
 
 type IGeneratedResponse = {
-    amount: number
-    scope: 'PARAGRAPH' | 'WORDS'
-    start: boolean
-    generated: string[]
-}
+	amount: number;
+	scope: 'PARAGRAPHS' | 'WORDS' | 'SENTENCES';
+	start: boolean;
+	generated: string[];
+};
 
 type EndpointOutput = {
-    status?: number
-    headers?: Headers
-    body?: IGeneratedResponse
-}
+	status?: number;
+	headers?: Headers;
+	body?: IGeneratedResponse;
+};
 
 export async function post(request): Promise<EndpointOutput> {
+	const body = request.body as IGenerate;
 
-    const body = request.body as IGenerate;
-
-    return {
-        body: {
-            amount: body.amount,
-            start: body.start,
-            scope: body.scope,
-            generated: await generate(body)
-        }
-    }
+	return {
+		body: {
+			amount: body.amount,
+			start: body.start,
+			scope: body.scope,
+			generated: await generate(body)
+		}
+	};
 }
-
-
-
-
-
-
-
